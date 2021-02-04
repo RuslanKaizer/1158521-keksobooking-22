09a357103fdd3cf7bbd5d7c1422f2019fb142f1b
RuslanKaizer <razorearth59@gmail.com>
2021-02-04 20:40:14 +0300

    Переделал функции

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
 'use strict'
 const getRandomNumber = function (min, max) {
-  max = Math.ceil(max);
-  min = Math.floor(min);
   const isValidCondition =  min < 0 || max < 0;
-  return isValidCondition ? alert('Значения не могут быть отрицательны') : Math.floor(Math.random() * (max - min + 1)) + min;
+  if (isValidCondition) {
+    alert('Значения не могут быть отрицательны');
+    return;
+  }
+  if (max < min) {
+    let buffer = max;
+    max = min;
+    min = buffer;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 const getRandomFloatingPoint = function (min, max, roundTo) {
   const isValidCondition = min < 0 || max < 0 || roundTo < 0;
-  return isValidCondition ? alert('Значения не могут быть отрицательны') : (Math.random() * (max - min) + min).toFixed(roundTo);
+  if (isValidCondition) {
+    alert('Значения не могут быть отрицательны');
+    return;
+  }
+  if (min > max){
+    let buffer = max;
+    max = min;
+    min = buffer;
+  }
+  if (roundTo > 5) {
+    roundTo = 5;
+  }
+  return min + ((Math.floor(((Math.random() * (max - min + 1))) * 10 ** roundTo)) / (10 ** roundTo));
 }
