e63105dc0d4389646b61e43216f3b2b8a498fcbf
RuslanKaizer <razorearth59@gmail.com>
2021-02-10 03:14:52 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,81 @@
 'use strict'
+const IMAGE_NUMBER = {
+  MIN: 1,
+  MAX: 8,
+};
+
+const TITLES = [
+  'The Pavilion',
+  'North + Vine',
+  'Elevate',
+  'The Residences at NewCity',
+];
+
+const PRICE_NUMBER = {
+  MIN: 0,
+  MAX: 1000000,
+};
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECKIN = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const DESCRIPTIOINS = [
+  'This room provide clean, comfortable lodging with full amenities, to make sure your stay is relaxing and stress-free.',
+  'A brand-new, elegantly designed, luxury apartment building located on a tree-lined street.',
+  'The Residences at NewCity is the perfect place to call your new home!',
+  'Suitable for singles, couple and travellers, you will not regret choosing this place.',
+];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+const NUMBERS = {
+  MIN: 0,
+  MAX: 100,
+};
+
+const COORDINATE_X = {
+  MIN: 35.65000,
+  MAX: 35.70000,
+};
+
+const COORDINATE_Y = {
+  MIN: 139.70000,
+  MAX: 139.80000,
+};
+
+const ROUND_FOR_LOCATIONS = 5;
+
+const ARRAY_LENGTH = 10;
+
 const getRandomNumber = function (min, max) {
   const isValidCondition =  min < 0 || max < 0;
   if (isValidCondition) {
@@ -29,5 +106,53 @@ const getRandomFloatingPoint = function (min, max, roundTo) {
   if (roundTo > 5) {
     roundTo = 5;
   }
-  return min + ((Math.floor(((Math.random() * (max - min + 1))) * 10 ** roundTo)) / (10 ** roundTo));
-}
+  return parseFloat((Math.random() * (max - min) + min).toFixed(roundTo));
+};
+
+const getRandomArrayElement = (elements) => {
+  return elements[getRandomNumber(0, elements.length - 1)]
+};
+
+const getUniqueArray = (elements) => {
+  const length = getRandomNumber(0, elements.length - 1);
+  const uniqueArray = [];
+  for (let i = 0; i < length; i++) {
+    uniqueArray.push(elements[i]);
+  }
+  return uniqueArray;
+};
+
+const getAuthor = () => ({
+  author: {
+    avatar: 'img/avatars/user0' + getRandomNumber(IMAGE_NUMBER.MIN,IMAGE_NUMBER.MAX) + '.png'
+  },
+});
+
+const getLocation = () => ({
+  x: getRandomFloatingPoint(COORDINATE_X.MIN, COORDINATE_X.MAX, ROUND_FOR_LOCATIONS),
+  y: getRandomFloatingPoint(COORDINATE_Y.MIN, COORDINATE_Y.MAX, ROUND_FOR_LOCATIONS),
+});
+
+const generateOffer = () => ({
+      title: getRandomArrayElement(TITLES),
+      address: Object.values(getLocation()),
+      price: getRandomNumber(PRICE_NUMBER.MIN, PRICE_NUMBER.MAX),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomNumber(NUMBERS.MIN, NUMBERS.MAX),
+      guests: getRandomNumber(NUMBERS.MIN, NUMBERS.MAX),
+      checkin: getRandomArrayElement(CHECKIN),
+      checkout: getRandomArrayElement(CHECKOUT),
+      features: getUniqueArray(FEATURES),
+      descriptions: getRandomArrayElement(DESCRIPTIOINS),
+      photos: getUniqueArray(PHOTOS),
+});
+
+const getTotalObject = () => ({
+  author: getAuthor(),
+  offer: generateOffer(),
+  location: getLocation(),
+});
+
+const createOffer = new Array(ARRAY_LENGTH).fill(null).map(getTotalObject);
+
+createOffer;
