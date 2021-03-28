2129c6780243778979b17361c2774efe7b7839ac
RuslanKaizer <razorearth59@gmail.com>
2021-03-28 23:43:27 +0300

    Защита

---
+++ source/js/main.js
@@ -0,0 +1,31 @@
+import { setAdFormSubmit, setAdFormReset } from './ad-form.js';
+import { setFilters, mapFiltersForm } from './map-form.js';
+import { addOffersToMap, clearMarkers } from './map.js';
+import { showAlert, disableForm, enableForm, debounce } from './util.js';
+import { getData } from './api.js';
+import { resetForm } from './reset-form.js';
+import { addPhotosListener } from './ad-form-photo.js';
+
+const RERENDER_DELAY = 500;
+
+getData(
+  (offers) => {
+    addOffersToMap(offers);
+    enableForm(mapFiltersForm, 'map__filters');
+    setFilters(debounce(
+      () => {
+        clearMarkers();
+        addOffersToMap(offers);
+      },
+      RERENDER_DELAY,
+    ));
+  },
+  (errorMessage) => {
+    disableForm(mapFiltersForm, 'map__filters');
+    showAlert(errorMessage);
+  },
+);
+
+setAdFormSubmit(resetForm);
+setAdFormReset(resetForm);
+addPhotosListener();
